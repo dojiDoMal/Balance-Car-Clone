f622cd746002ea149a471fef8ee7abc753f6fd45
dojiDoMal <andersonamacedo@hotmail.com>
2019-09-29 14:17:08 -0300

    Projeto Final

---
+++ BalanceCar/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 newPos = target.position;
+		newPos.z = -10f;
+		transform.position = newPos;
+	}
+}
---
+++ BalanceCar/Assets/Scripts/CarController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+	public float speed = 1500f;
+	public float rotationSpeed = 15f;
+
+	public WheelJoint2D backWheel, frontWheel;
+	public Rigidbody2D rb;
+
+	private float rotation = 0f;
+	private float movement = 0f;
+
+	void Update() {
+		movement = Input.GetAxisRaw("Vertical") * -speed ;
+		rotation = Input.GetAxisRaw("Horizontal");
+
+		
+	}
+
+	void FixedUpdate() {
+		if (movement == 0f)
+		{
+			backWheel.useMotor = false;
+			frontWheel.useMotor = false;
+		}
+		else {
+			backWheel.useMotor = true;
+			frontWheel.useMotor = true;
+			JointMotor2D motor = new JointMotor2D { motorSpeed = movement, maxMotorTorque = backWheel.motor.maxMotorTorque };
+			backWheel.motor = motor;
+			frontWheel.motor = motor;
+		}
+
+		rb.AddTorque(-rotation * rotationSpeed * Time.fixedDeltaTime);
+
+	}
+
+}
